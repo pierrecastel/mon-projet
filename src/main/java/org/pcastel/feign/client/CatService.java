a1ba174f76f3c34b0e349d1154e1484a784f722a
Pierre Castel <pierre.castel@capgemini.com>
2017-07-25 01:59:03 +0200

    delete part of CatService code

--- src/main/java/org/pcastel/feign/client/CatService.java
+++ src/main/java/org/pcastel/feign/client/CatService.java
@@ -56,26 +56,6 @@ public class CatService {
 
     public void playWithCats() {
 
-        // Find All
-        List<Cat> cats = client.findAll();
-        cats.forEach(System.out::println);
-        System.out.println();
-
-        // Find by id
-        String id = cats.stream().map(Cat::getId).findFirst().orElse(null);
-        Cat c = client.findById(id);
-        System.out.println("Cat: " + c);
-        System.out.println();
-
-        // Create
-        Cat newCat = new Cat("Felix", Cat.CatRace.Angora);
-        Cat created = client.create(newCat);
-        System.out.println("New Cat: " + created);
-        System.out.println();
-
-        // Delete
-        Cat deleted = client.delete(created.getId());
-        System.out.println("Deleted cat: " + deleted);
-        System.out.println();
+        
     }
 }
