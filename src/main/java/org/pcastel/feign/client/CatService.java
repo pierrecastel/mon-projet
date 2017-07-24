fff41bb19f3754c04995a0f1dbc43813464e8a7b
Pierre Castel <pcastel@LFR006086.corp.capgemini.com>
2017-07-25 00:42:51 +0200

    refacto catService

---
+++ src/main/java/org/pcastel/feign/client/CatService.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2012, vsc-technologies - www.voyages-sncf.com
+ * All rights reserved.
+ * 
+ * Les presents codes sources sont proteges par le droit d'auteur et 
+ * sont la propriete exclusive de VSC Technologies.
+ * Toute representation, reproduction, utilisation, exploitation, modification, 
+ * adaptation de ces codes sources sont strictement interdits en dehors 
+ * des autorisations formulees expressement par VSC Technologies, 
+ * sous peine de poursuites penales. 
+ * 
+ * Usage of this software, in source or binary form, partly or in full, and of
+ * any application developed with this software, is restricted to the
+ * customer.s employees in accordance with the terms of the agreement signed
+ * with VSC-technologies.
+ */
+package org.pcastel.feign.client;
+
+import static java.time.Instant.now;
+import java.util.List;
+
+import feign.Feign;
+import feign.Logger;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.slf4j.Slf4jLogger;
+
+/**
+ */
+public class CatService {
+    
+    
+    private String url;
+    private CatClient client;
+
+    public CatService() {
+
+        url = "http://localhost:8080";
+
+        // Get Client
+        client = Feign.builder()
+                // Transform response body
+                .decoder(new GsonDecoder())
+                        // Create request body
+                .encoder(new GsonEncoder())
+                        // Add custom header
+                .requestInterceptor(template -> template.header("Date", now().toString()))
+                        // Tips debug lite
+                .requestInterceptor(System.out::println)
+                        // Tips debug logger
+                .logLevel(Logger.Level.FULL)
+                .logger(new Slf4jLogger())
+                        // Create instance
+                .target(CatClient.class, url);
+    }
+
+    public void playWithCats() {
+
+        // Find All
+        List<Cat> cats = client.findAll();
+        cats.forEach(System.out::println);
+        System.out.println();
+
+        // Find by id
+        String id = cats.stream().map(Cat::getId).findFirst().orElse(null);
+        Cat c = client.findById(id);
+        System.out.println("Cat: " + c);
+        System.out.println();
+
+        // Create
+        Cat newCat = new Cat("Felix", Cat.CatRace.Angora);
+        Cat created = client.create(newCat);
+        System.out.println("New Cat: " + created);
+        System.out.println();
+
+        // Delete
+        Cat deleted = client.delete(created.getId());
+        System.out.println("Deleted cat: " + deleted);
+        System.out.println();
+    }
+}
